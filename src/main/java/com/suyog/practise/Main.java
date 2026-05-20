b80c43ebbb3e06e8b2bbe9b684cde78bd6d00855
Suyog Nimbre <heisenbergnimbre@gmail.com>
2026-05-20 19:55:12 +0530

    task 3

--- src/main/java/com/suyog/practise/Main.java
+++ src/main/java/com/suyog/practise/Main.java
@@ -6,5 +6,11 @@ public class Main {
         Employee emp1 = new Employee("Suyog",29,45000,"IQE","suyognimbre@infosys.com");
         emp1.displayInfo();
         emp1.incrementSalary(5000);
+
+        Employee emp2 = new Employee("mayur",33,45000,"IQE","mayurpawar@infosys.com");
+        Employee emp3 = new Employee("mangesh",44,90000,"SAP","mangeshkulkrani@infosys.com");
+        Employee emp4 = new Employee("sangram",40,70000,"SAP","sangramdesai@infosys.com");
+        Employee emp5 = new Employee("shankar",40,80000,"Java","shankardongre@infosys.com");
+
     }
 }
