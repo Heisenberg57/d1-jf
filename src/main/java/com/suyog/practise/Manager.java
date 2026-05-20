b4b65345054773c4561b74e271374e69391c2e0b
Suyog Nimbre <heisenbergnimbre@gmail.com>
2026-05-20 20:00:47 +0530

    task 5

---
+++ src/main/java/com/suyog/practise/Manager.java
@@ -0,0 +1,10 @@
+package com.suyog.practise;
+
+public class Manager {
+    int teamSize;
+
+    public Manager(int teamSize){
+        this.teamSize=teamSize;
+        System.out.println("The team size of assigned manager is : "+teamSize);
+    }
+}
